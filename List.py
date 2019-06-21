6b3c64a5e4f2d496be3724067622ec0f0ecdd9e6
unknown <you@example.com>
2019-06-21 09:01:57 +0530

    new questions

---
+++ List.py
@@ -0,0 +1,7 @@
+l1=[12,45,35,17,6,9]
+l2=["Ram","shyam","Rishi","shivu","Gaurav","rekha"]
+print(l1)
+l1.sort()
+print(l1)
+l2.sort()
+print(l2)
--- Series_Question.py
+++ Series_Question.py
@@ -10,3 +10,15 @@ for c in range(1,6):
         print(n,end=" ")
         n=n+1
     print("\n")
+print("\n\n\t Series 3")
+for i in range(1,6):
+        for k in range(1,i):
+            print("*",end=" ")
+        print("\n")
+print("\n\n\t Series 4")
+for e in range(1,5):
+    for f in range(4,0,-1):
+        print(" ")
+    for g in range(1,5):
+        print(" $ ",end=" ")
+    print("\n")
